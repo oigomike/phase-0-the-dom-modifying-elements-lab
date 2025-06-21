602af56539b71cc741b86dcda9771275207465ec
oigo <michaelbichage2000@gmail.com>
2025-06-21 21:54:59 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';  
+newHeader.textContent = "Your Name is the champion";  
+document.body.appendChild(newHeader);
+
+document.getElementById('main').remove();
+
